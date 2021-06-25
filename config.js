bfcbef458f384be522aeb8a666cdadbd38c78047
George Clark <clarkgeorge76@gmail.com>
2021-06-25 11:33:43 +0100

    algolia search config

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ const config = {
     links: [{ text: '', link: '' }],
     search: {
       enabled: true,
-      indexName: 'test_cardano-docs',
+      indexName: 'cardano_docs',
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
       algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
       algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
