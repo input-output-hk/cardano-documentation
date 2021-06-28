4710915569abb24968eba060560ac5412dbd71d3
George Clark <clarkgeorge76@gmail.com>
2021-06-28 15:24:34 +0100

    move algolia search to webdev account

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ const config = {
     links: [{ text: '', link: '' }],
     search: {
       enabled: true,
-      indexName: 'cardano_docs',
+      indexName: 'cardano_documentation_search_index',
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
       algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
       algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
