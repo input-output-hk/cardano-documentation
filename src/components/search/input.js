3883ab9f5f69c1f332745ff94f83a54e93a9ee16
George Clark <clarkgeorge76@gmail.com>
2021-07-06 12:11:39 +0100

    amend timer

--- src/components/search/input.js
+++ src/components/search/input.js
@@ -75,7 +75,7 @@ const Form = styled.form`
 
 export default connectSearchBox(({ refine, ...rest }) => {
   const [searchValue, setSearchValue] = useState('')
-  const [debouncedSearchValue] = useDebounce(searchValue, 2000)
+  const [debouncedSearchValue] = useDebounce(searchValue, 1000)
    
   const handleChange = (e) => {
     setSearchValue(e.target.value)
