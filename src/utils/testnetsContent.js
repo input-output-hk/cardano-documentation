14d9ddf75f42e91d405c943b05b349f02cd3f880
Martin Hunt <martin.hunt@iohk.io>
2022-08-10 10:50:57 +0100

    add one more external url and delete un-needed testnet copy

--- src/utils/testnetsContent.js
+++ src/utils/testnetsContent.js
@@ -1,24 +1,7 @@
-// This is the content moved over from testnets site..
-// TODO: keep only what's required and store in markdown
+// This is the content moved over from testnets site that was stored in Markdown. Is used for Faucet,
+// downloaders and staking calculator
 
 export default {
-  main_title: 'Cardano Testnets',
-  main_title_aria_label: 'Cardano Testnets Home',
-  select_language: 'Select language',
-  select_theme: 'Select theme',
-  last_updated: 'Last updated',
-  report_an_issue: 'Report a documentation issue',
-  search: 'Search',
-  previous: 'Previous',
-  next: 'Next',
-  search_form_aria_label: 'Search Cardano Testnets',
-  search_form_submit_aria_label: 'Perform search',
-  open_search_bar: 'Open search bar',
-  close_search_bar: 'Close search bar',
-  main_navigation_label: 'Main',
-  close_main_navigation: 'Close main navigation menu',
-  open_main_navigation: 'Open main navigation menu',
-  logo_alt: 'Cardano Testnets Logo',
   staking_calculator: {
     saturation: 'Pool saturation',
     select_a_calculator: 'Select a calculator',
