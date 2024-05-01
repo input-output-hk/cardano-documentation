3b89a79a671832738a192f2d3ad199812372d9a8
Filip Stoyanov <filip.stoyanov@iohk.io>
2024-05-01 16:33:58 +0100

    filip(feat): abstract redirects to separate file for easier management

---
+++ redirects.js
@@ -0,0 +1,10 @@
+export const redirects = [
+  {
+    to: '/cardano-testnets/tools/faucet',
+    from: '/cardano-testnet/tools/faucet',
+  },
+  {
+    to: '/release-notes/comp-matrix',
+    from: '/tools/comp-matrix',
+  },
+]
