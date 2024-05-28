72a4677292cbcdef622b6a367169636238219cfc
Luke Martin <luke.martin@iohk.io>
2024-05-28 13:43:46 +0100

    fix: Remove .docusaurus generated dir

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/fstoqnov/cardano-documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/fstoqnov/cardano-documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/fstoqnov/cardano-documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/fstoqnov/cardano-documentation/src/css/custom.css"),
-];
